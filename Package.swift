3c7f41e4db384355e17b980d49e990c396038e2a
mckomo <mckomo@gmail.com>
2016-09-20 16:06:03 +0200

    Added config
    Added config to enable custom build and start commands

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Hello",
+    name: "gulp-vapor",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0)
     ],
---
+++ test/config.spec.babel.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// require('babel-core/register');
+
+import {expect, assert} from 'chai';
+import Config from '../scripts/config';
+
+describe('Config', () => {
+
+    it('stores commands', () => {
+      expect(Config.commands).be.an('object');
+    });
+
+    describe('.commands', () => {
+
+        describe('.start ', () => {
+
+          const _startCommand = Config.commands.start;
+
+          it('is stored as array', () => {
+            expect(_startCommand).be.a('array');
+          })
+
+          it('holds program path as first element', () => {
+            expect(_startCommand[0]).to.eq('.build/debug/gulp-vapor');
+          })
+
+          it('holds array of program arguments as second element', () => {
+            expect(_startCommand[1]).to.have.members(['serve']);
+          })
+
+        });
+
+        describe('.build ', () => {
+
+          const _buildCommand = Config.commands.build;
+
+          it('is stored as array', () => {
+            expect(_buildCommand).be.a('array');
+          })
+
+          it('holds default swift build command', () => {
+            expect(_buildCommand).to.have.members(['swift', 'build']);
+          })
+
+        });
+
+    });
+
+    describe('.projectName', () => {
+
+      const _projectName = Config.projectName;
+
+      it('is equal to the project directory name', () => {
+        expect(_projectName).to.eq('gulp-vapor')
+      });
+
+    });
+
+});
